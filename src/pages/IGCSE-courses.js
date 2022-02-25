248e927a1fadf277115101d6d7c4ad3dab8afc69
cobatsu <huze.ozr@gmail.com>
2022-02-25 19:50:11 +0100

    test

--- src/pages/IGCSE-courses.js
+++ src/pages/IGCSE-courses.js
@@ -1,27 +1,27 @@
 import * as React from "react";
-import {Courses} from "../components/common/courses";
+import { Courses } from "../components/common/courses";
 import * as styles from "../../styles/pages/home.module.css";
-import {graphql} from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "../components/layoutelements/layout";
-
+//heyyo lan
 const IGCSECourses = (props) => {
-    const {data: {
-            allMarkdownRemark
-        }} = props;
-    return (
-        <Layout title="IGCSE Courses">
-            <div className={
-                styles.main_wrapper
-            }>
-                <Courses allMarkdownRemark={allMarkdownRemark}
-                    title="IGCSE Courses"
-                    type="IGCSE"/>
-            </div>
-        </Layout>
-    );
+  const { data: {
+    allMarkdownRemark
+  } } = props;
+  return (
+    <Layout title="IGCSE Courses">
+      <div className={
+        styles.main_wrapper
+      }>
+        <Courses allMarkdownRemark={allMarkdownRemark}
+          title="IGCSE Courses"
+          type="IGCSE" />
+      </div>
+    </Layout>
+  );
 };
 
-export const query = graphql `
+export const query = graphql`
   query getAllPosts3 {
     allMarkdownRemark {
       nodes {

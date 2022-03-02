89a6df48b0faf11fdca8d80a4a13395813a0d4f9
cobatsu <huze.ozr@gmail.com>
2022-03-02 21:00:29 +0100

    changed url

--- src/components/common/applyForm.js
+++ src/components/common/applyForm.js
@@ -150,7 +150,7 @@ const RefferenceNumber = ({ courseApplied, levelApplied, courseFee }) => {
 
         axios
             .post(
-                "http://localhost:3001/api/register/addNewOnlineStudent",
+                "https://study-online.herokuapp.com/api/register/addNewOnlineStudent",
                 {
                     scholarShipInfo: contactForm,
                     studentModel: initialStudent,

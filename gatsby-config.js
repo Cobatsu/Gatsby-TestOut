77e87267b282553ee6df279a4c945ed9efe34c98
cobatsu <huze.ozr@gmail.com>
2022-06-06 20:06:21 -0500

    lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,34 @@
 module.exports = {
- siteMetadata: {
-  title: "My Gatsby Site",
- },
- plugins: [
-  "gatsby-plugin-sharp",
-  "gatsby-transformer-sharp",
-  "gatsby-image",
-  "gatsby-transformer-remark",
-  "gatsby-plugin-react-head",
-  `gatsby-plugin-styled-components`,
-  "gatsby-plugin-use-query-params",
-  {
-   resolve: `gatsby-source-filesystem`,
-   options: {
-    name: `courses`,
-    path: `${__dirname}/src/courses/`,
-   },
-  },
-  {
-   resolve: `gatsby-plugin-nprogress`,
-   options: {
-    // Setting a color is optional.
-    color: `rgb(251, 201, 51)`,
-    // Disable the loading spinner.
-    showSpinner: false,
-   },
-  },
-  {
-   resolve: `gatsby-source-filesystem`,
-   options: {
-    path: `${__dirname}/src/images`,
-   },
-  },
- ],
+    siteMetadata: {
+        title: "My Gatsby Site",
+    },
+    plugins: [
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-image",
+        "gatsby-transformer-remark",
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `courses`,
+                path: `${__dirname}/src/courses/`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-nprogress`,
+            options: {
+                // Setting a color is optional.
+                color: `rgb(251, 201, 51)`,
+                // Disable the loading spinner.
+                showSpinner: false,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/images`,
+            },
+        },
+    ],
 };
